a656b45ab9e929c1ce14cd0955e1952d64fb083a
IainMcHugh <iamchugh@tcd.ie>
2021-06-26 21:37:03 +0100

    added transition to sticky button wrapper on mobile

--- revaamp/static/js/main.js
+++ revaamp/static/js/main.js
@@ -1,3 +1,22 @@
+$.fn.isOnScreen = function(){
+
+    var win = $(window);
+
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
+
 $(document).ready(() => {
     let isOpen = false;
     $('.toggle-dropdown').on('click', () => {
@@ -16,5 +35,23 @@ $(document).ready(() => {
             $('.header-dropdown').show();
             isOpen = true;
         }    
+    });
+
+    let isFooterVisible = false;
+
+    $(window).scroll(function(){
+        if ($('footer').isOnScreen()) {
+            // The element is visible, do something
+            if(!isFooterVisible){ 
+                $('.sticky-buttons-wrapper').css('transform', 'translateY(56px)');
+                isFooterVisible = true;
+            }
+        } else {
+            // The element is NOT visible, do something else
+            if(isFooterVisible){
+                $('.sticky-buttons-wrapper').css('transform', 'translateY(0)');
+                isFooterVisible = false;
+            }
+        }
     });
 })
