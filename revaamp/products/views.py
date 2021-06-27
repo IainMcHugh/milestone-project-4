fbced1a6f3d8fe3a642326d072ca5b4ca747e2b6
IainMcHugh <iamchugh@tcd.ie>
2021-06-27 14:40:05 +0100

    added sorting

--- revaamp/products/views.py
+++ revaamp/products/views.py
@@ -14,8 +14,22 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if "sort" in request.GET:
+            sortKey = request.GET["sort"]
+            sort = sortKey
+            if sortKey == "name":
+                sortKey == "lower_name"
+                products = products.annotate(lower_name=Lower("name"))
+            if "direction" in request.GET:
+                direction = request.GET["direction"]
+                if direction == "desc":
+                    sortKey = f"-{sortKey}"
+            products = products.order_by(sortKey)
+
         if "category" in request.GET:
             categories = request.GET["category"].split(",")
             products = products.filter(category__name__in=categories)
@@ -30,10 +44,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f"{sort}_{direction}"
+
     context = {
         "products": products,
         "search_term": query,
         "current_categories": categories,
+        "current_sorting": current_sorting,
     }
 
     return render(request, "products/products.html", context)
