d833ffbe4cc10fdb7ea88065f19b16d67e9a526a
IainMcHugh <iamchugh@tcd.ie>
2021-07-06 21:06:56 +0100

    Added toasts in cart view

--- revaamp/cart/views.py
+++ revaamp/cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -16,13 +16,16 @@ def add_to_cart(request, item_id):
     Add a quantity of a specified product to the shopping bag
     """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get("quantity"))
     redirect_url = request.POST.get("redirect_url")
     cart = request.session.get("cart", {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {cart[item_id]}')
+
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {product.name} to your cart.')
@@ -52,12 +55,15 @@ def remove_from_cart(request, item_id):
     """
     Remove an item from the shopping cart
     """
+    product = get_object_or_404(Product, pk=item_id)
     try:
-        print(item_id)
         cart = request.session.get("cart", {})
         cart.pop(item_id)
-
+        messages.success(
+            request, f'Removed {product.name} from cart')
         request.session["cart"] = cart
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(
+            request, f'Oops! Something went wrong. {e}')
         return HttpResponse(status=500)
