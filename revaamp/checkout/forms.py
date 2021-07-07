a0283c3154dccd33ecd1dcb258b6ad15e752b81e
IainMcHugh <iamchugh@tcd.ie>
2021-07-07 22:27:54 +0100

    added checkout views and templates

--- revaamp/checkout/forms.py
+++ revaamp/checkout/forms.py
@@ -5,10 +5,10 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fiels = ('full_name', 'email', 'phone_number',
-                 'street_address1', 'street_address2',
-                 'town_or_city', 'postcode', 'country',
-                 'county',)
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
     def __init__(self, *args, **kwargs):
         """
