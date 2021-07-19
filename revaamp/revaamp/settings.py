776c2cc7fb85d7238819199fdd258f7ab2d51513
IainMcHugh <iamchugh@tcd.ie>
2021-07-19 22:34:59 +0100

    set DEBUG in settings

--- revaamp/revaamp/settings.py
+++ revaamp/revaamp/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECRET_KEY = "django-insecure-n@(eh7j$+x)%f1hjde($(si_64ldod+41^e)svy+gbzfj*rhei"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = "DEVELOPMENT" in os.environ
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
+
 
 ALLOWED_HOSTS = ["revaamp.herokuapp.com", "localhost"]
 
