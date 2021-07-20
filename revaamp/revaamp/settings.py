b71162c649eaa5bc92cff3a67e6138130a85b98f
IainMcHugh <iamchugh@tcd.ie>
2021-07-20 21:32:18 +0100

    sweep of style updates and refactoring

--- revaamp/revaamp/settings.py
+++ revaamp/revaamp/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECRET_KEY = "django-insecure-n@(eh7j$+x)%f1hjde($(si_64ldod+41^e)svy+gbzfj*rhei"
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = "DEVELOPMENT" in os.environ
 
 
